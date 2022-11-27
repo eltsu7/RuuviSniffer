6188636fb8817c3937598d5ac254fc021f682a58
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2022-11-27 23:32:35 +0200

    jotain

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from ruuvitag_sensor.ruuvi import RuuviTagSensor
+import logging
 from datetime import datetime
+import sys
 
-from config import UPDATE_TIMEOUT
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+log = logging.Logger("ruuvisniffer")
+log.addHandler(logging.StreamHandler(sys.stdout))
+
+from config import UPDATE_TIMEOUT, INFLUX_TOKEN, INFLUX_HOST, INFLUX_ORG, INFLUX_BUCKET
 
     
 class RuuviSniffer:
     def __init__(self) -> None:
         self.latest_update: datetime = datetime.now()
         self.data: dict = {}
+        self.database = InfluxDBClient(url=INFLUX_HOST, token=INFLUX_TOKEN, org=INFLUX_ORG) \
+                .write_api(write_options=SYNCHRONOUS)
 
     def start(self):
         RuuviTagSensor.get_data(self.handle_data)
 
     def handle_data(self, bluetooth_data):
-        print(f"Got data: {bluetooth_data}")
-        mac = bluetooth_data[0]
+        mac = bluetooth_data[0].replace(':','')
         sensor_data = bluetooth_data[1]
         self.data[mac] = sensor_data
 
@@ -22,9 +31,18 @@ class RuuviSniffer:
             self.upload_data()
 
     def upload_data(self):
-        print(f"Uploading data..")
-        self.latest_update = datetime.now()
+        for mac, data in self.data.items():            
+            point = Point("ruuvi_measurements")
+            for measurement in data:
+                point.field(field=measurement, value=data[measurement])
+
+            point.tag(key="mac", value=mac)
+
+            self.database.write(INFLUX_BUCKET, record=point)
+
+        log.info(f"{datetime.now()} - Sent data from: {self.data.keys()}")
         self.data = {}
+        self.latest_update = datetime.now()
 
     
 if __name__ == '__main__':
